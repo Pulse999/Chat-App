fda33a0b4de1e76a1c2f53643edb7578829893b8
Thembinkosi Kene <148543913+Pulse999@users.noreply.github.com>
2025-08-30 18:27:10 +0200

    Removed the mongoose issue

--- client/src/Components/ChatContainer.jsx
+++ client/src/Components/ChatContainer.jsx
@@ -3,7 +3,6 @@ import assets, { messagesDummyData } from "../assets/assets";
 import { formatMessageTime } from "../lib/utils";
 import { ChatContext } from "../../context/ChatContext";
 import { AuthContext } from "../../context/AuthContext";
-import { set } from "mongoose";
 import toast from "react-hot-toast";
 
 const ChatContainer = () => {
