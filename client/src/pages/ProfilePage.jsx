ca760b2b032f590b3c90e60d722a721c50c8c290
Thembinkosi Kene <148543913+Pulse999@users.noreply.github.com>
2025-09-28 01:55:57 +0200

    added buddy on profile page

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -51,7 +51,7 @@ const ProfilePage = () => {
           rounded-full text-lg cursor-pointer'>Save</button>
         </form>
         <img className={`max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10 ${selectedImg && 'rounded-full'}`} 
-        src={authUser?.profilePic || assets.logo_icon} alt="" />
+        src={authUser?.profilePic || assets.buddy} alt="" />
       </div>
     </div>
   )
